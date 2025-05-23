36188880acf1cecf8e2d7d41dd492c6156b3a118
huyghebaertthomas <thomas.huyghebaert@howest.be>
2025-05-23 13:10:54 +0000

    feat: added docker image config

--- .devcontainer/scripts/post_create.sh
+++ .devcontainer/scripts/post_create.sh
@@ -3,6 +3,7 @@
 # Prompt user to create virtual environments for TensorFlow and/or PyTorch
 clear
 
+bash .devcontainer/scripts/change_image_name.sh
 bash .devcontainer/scripts/create_python_env.sh "$1"
 
 # Feel free to add setup commands here, they will run once after (re)building the devcontainer
